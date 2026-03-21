792e528270a0d12a18c0e663a942eab65445c426
anton <antondurov05@protonmail.com>
2026-03-21 18:58:07 -0400

    Updated emotion_detections.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,5 +6,28 @@ def emotion_detector(text_to_analyze):
     input_json = {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(url, json=input_json, headers=headers)
+    result = response.json()
 
-    return response.text
+    emotions = result["emotionPredictions"][0]["emotion"]
+    anger_score   = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score    = emotions["fear"]
+    joy_score     = emotions["joy"]
+    sadness_score = emotions["sadness"]
+    emotion_scores = {
+        'anger':   anger_score,
+        'disgust': disgust_score,
+        'fear':    fear_score,
+        'joy':     joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    return {
+        'anger':            anger_score,
+        'disgust':          disgust_score,
+        'fear':             fear_score,
+        'joy':              joy_score,
+        'sadness':          sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
